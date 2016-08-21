9a9f3bfa952228992bf3af183f22b391b05d83ec
Ronnie <rix17172@gmail.com>
2016-08-21 13:06:13 +0200

    Update stackoverflow.js

--- stackoverflow.js
+++ stackoverflow.js
@@ -13,6 +13,5 @@ cronwork("stackoverflow.update", function(){
   config_file_create("last_updatet", new Date().getTime());
   var query = "https://api.stackexchange.com/2.2/questions?fromdate="+update+"&order=desc&sort=creation&site=stackoverflow";
   var con = new Http(query);
-  var xml = new DOMParser();
-  xml.parseFromString(con.toString());
+  var data = JSON.parse(con.toString ());
 }, "1m");
