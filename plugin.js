cb6315f601acda1fce880ddb07abbe228381fef8
Ronnie <rix17172@gmail.com>
2016-08-22 12:15:47 +0200

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -3,6 +3,23 @@ This is a system plugin. It means it has more access end normally plugin.
 This plugin handle the job to controle update for plugin. It will also update a plugin if it is meaning!
 */
 
+function getData(list){
+ var r = {};
+ for(var i=0;i<list.length;i++){
+    r[list[i].name] = list[i];
+ }
+
+ return r;
+}
+
 cronwork("plugin.controler", function(){
   var html = new Http("https://api.github.com/repos/Jugolo/Plugin/contents/");
+  var data = getData(JSON.parse(html.exec().toString()));
+  var dir = new DirList("include/plugin/");
+  var current = "";
+  while(current = dir.next(false)){
+    if(current.isDir()){
+       
+    }
+  }
 }, "1d);//do it every day (24 hours interval)
