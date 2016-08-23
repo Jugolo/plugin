817c0bf4dc07ef0567d4b49fcaa4aa8e96660d35
Ronnie <rix17172@gmail.com>
2016-08-23 12:17:51 +0200

    Update stackoverflow.js

--- stackoverflow/stackoverflow.js
+++ stackoverflow/stackoverflow.js
@@ -5,13 +5,13 @@ useNick("stackoverflow");
 cronwork("stackoverflow.update", function(){
   if(!conf_file_exists("last_updatet")){
      //create it and wait 1 min
-     conf_file_create("last_updatet", new Date().getTime());
+     conf_file_create("last_updatet.cow_config", new Date().getTime());
      return;
   }
 
   //wee get last update
-  var update = conf_file_get("last_updatet");
-  config_file_create("last_updatet", new Date().getTime());
+  var update = conf_file_get("last_updatet.cow_config");
+  config_file_create("last_updatet.cow_config", new Date().getTime());
   var query = "https://api.stackexchange.com/2.2/questions?fromdate="+update+"&order=desc&sort=creation&site=stackoverflow";
   var con = new Http(query);
   var result = con.exec();
